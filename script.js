8f2ed45a9252f9a01d589caab6fbdcaf66ced387
reuben-idan <nobleadonis2@gmail>
2024-05-23 22:58:31 +0000

    Update script.js file

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getRandomColor() {
 }
 
 // Function to apply random colors to an element
-function applyRandomColors(element) {
+function changeColors(element) {
   const color1 = getRandomColor();
   const color2 = getRandomColor();
   const color3 = getRandomColor();
@@ -15,11 +15,3 @@ function applyRandomColors(element) {
   element.style.listStyleColor = color2; // Change list style color
   element.style.borderColor = color3; // Add a border with random color
 }
-
-// Add event listener to all h5 elements
-const h5Elements = document.querySelectorAll('h5');
-h5Elements.forEach(h5 => {
-  h5.addEventListener('click', () => {
-    applyRandomColors(h5);
-  });
-});
